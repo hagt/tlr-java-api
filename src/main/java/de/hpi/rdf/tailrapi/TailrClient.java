e13cfe96bc042294d24811acf80417d125924676
Henning Agt-Rickauer <henning.agt-rickauer@hpi.de>
2016-07-13 10:56:45 +0200

    Update TailrClient.java

--- src/main/java/de/hpi/rdf/tailrapi/TailrClient.java
+++ src/main/java/de/hpi/rdf/tailrapi/TailrClient.java
@@ -16,8 +16,8 @@ import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.BasicHttpEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
 
 import java.io.*;
